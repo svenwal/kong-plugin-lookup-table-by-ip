065ee7c497f096b88d7f3870a1a5a319964275c2
Sven Walther <sven@svenwal.de>
2024-11-13 10:50:53 +0000

    Caching added

--- kong/plugins/kong-plugin-lookup-table-by-ip/handler.lua
+++ kong/plugins/kong-plugin-lookup-table-by-ip/handler.lua
@@ -2,6 +2,7 @@ local lookuptablebyip = {
     PRIORITY = 1010, -- set the plugin priority, which determines plugin execution order
     VERSION = "0.9",
   }
+local ipmatcher = require("resty.ipmatcher")
 
 function lookuptablebyip:access(config)
     local ip
@@ -12,13 +13,40 @@ function lookuptablebyip:access(config)
     end
 
     if not ip then
-        kong.log.info("No IP address found")
+        kong.log.err("No IP address found")
+	kong.response.exit(config.fail_status_code, config.fail_status_message, {["Content-Type"] = config.fail_status_content_type})
     end
 
     kong.log.debug("IP found: " .. ip)
 
     local header_value = ""
-    local ipmatcher = require("resty.ipmatcher")
+    local ip_cache_key = "ip-lookup-" .. ip
+    local opts = { ttl = config.cache_ttl }
+
+    local header_value, err = kong.cache:get(ip_cache_key, opts, lookup_ip, config, ip)
+
+    if err then
+      kong.log.err(err)
+	if config.fail_on_missing_lookup then
+	    kong.response.exit(config.fail_status_code, config.fail_status_message, {["Content-Type"] = config.fail_status_content_type})
+        end 
+    end
+
+    if header_value == "" then
+	if config.fail_on_missing_lookup then
+	    kong.response.exit(config.fail_status_code, config.fail_status_message, {["Content-Type"] = config.fail_status_content_type})
+	else 
+            header_value = config.default_value_if_lookup_fails
+	end
+    end
+
+    kong.log.debug("Setting header " .. config.header_name .. " to " .. header_value)
+    kong.service.request.set_header(config.header_name, header_value)
+end
+
+function lookup_ip(config, ip)
+    kong.log.debug("Looking up IP " .. ip)
+    local header_value = ""
     local range_index = 1
     for index,ranges in ipairs(config.ip_ranges) do
         local range = {}
@@ -33,15 +61,8 @@ function lookuptablebyip:access(config)
 	range_index = range_index + 1
     end
 
-    if header_value == "" then
-	if config.fail_on_missing_lookup then
-	    kong.response.exit(config.fail_status_code, config.fail_status_message, {["Content-Type"] = config.fail_status_content_type})
-	else 
-            header_value = config.default_value_if_lookup_fails
-	end
-    end
-
-    kong.service.request.set_header(config.header_name, header_value)
+    kong.log.debug("Header value is found: " .. header_value)
+    return header_value
 end
 
 return lookuptablebyip
