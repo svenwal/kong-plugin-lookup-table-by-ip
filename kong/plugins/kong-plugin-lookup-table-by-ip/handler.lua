d746c982ed2160475f2e1766f302d993fafbdc7b
Sven Walther <sven@svenwal.de>
2024-11-13 08:38:02 +0000

    First working code, refinements needed, caching should be added

---
+++ kong/plugins/kong-plugin-lookup-table-by-ip/handler.lua
@@ -0,0 +1,40 @@
+local lookuptablebyip = {
+    PRIORITY = 1010, -- set the plugin priority, which determines plugin execution order
+    VERSION = "0.9",
+  }
+
+function lookuptablebyip:access(config)
+    local ip
+    if not config.use_header_instead_of_real_ip then
+        ip = ngx.var.remote_addr
+    else
+        ip = kong.request.get_header(config.header_instead_of_ip)
+    end
+
+    if not ip then
+        kong.log.info("No IP address found")
+    end
+
+    kong.log.debug("IP found: " .. ip)
+
+    local header_value = ""
+    local ipmatcher = require("resty.ipmatcher")
+    local range_index = 1
+    for index,ranges in ipairs(config.ip_ranges) do
+        local range = {}
+        for value in string.gmatch(ranges, "([^,]+)") do
+            table.insert(range, value)
+        end
+        local matcher = ipmatcher.new(range)
+        if matcher:match(ip) then
+            kong.log.debug("IP is in CIDR " .. range_index)
+	    header_value = config.header_value[range_index]
+        end
+	range_index = range_index + 1
+    end
+
+    kong.service.request.set_header(config.header_name, header_value)
+end
+
+return lookuptablebyip
+
---
+++ kong/plugins/kong-plugin-lookup-table-by-ip/schema.lua
@@ -0,0 +1,20 @@
+return {
+  name = "lookup-table-by-ip",
+  fields = {
+    { config = {
+        type = "record",
+        fields = {
+                { use_header_instead_of_real_ip = { type = "boolean",required = false, default = false}, },
+                { header_instead_of_ip = { type = "string",required = false,  default = "x-forwarded-ip"}, },
+                { header_name = { type = "string",required = true,  default = "x-kong-lookup"}, },
+		{ ip_ranges = { type = "array", required = true, default = {"application/xml"}, elements = { type = "string"}, }, },
+		{ header_value = { type = "array", required = true, default = {"application/xml"}, elements = { type = "string"}, }, },
+                { fail_on_missing_lookup = { type = "boolean",required = false, default = false}, },
+        },
+      },
+    },
+  },
+  entity_checks = {
+    -- Add any checks here if needed
+  },
+}
